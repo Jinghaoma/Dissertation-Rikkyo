d4197d9eb35aa02eddc9c49b49298bbc978083b8
jinghaom <jinghaom@gmail.com>
2018-01-10 22:42:16 +0900

    初版完成

--- 下書き.R
+++ 下書き.R
@@ -1,1 +1,15 @@
 x = c(rnorm(50),rnorm(50,1),rnorm(50,2))
+f = gl(3,50)
+ao = aov(x~f)
+ao = summary(ao)
+ao
+
+tt = c()
+for (i in 1:10000) {
+  n = 50
+  t = rnorm(50,10,10)
+  t1 = sd(t)^2*(n-1)
+  #t1 = sum((t-mean(t))^2)
+  tt = c(tt,t1)
+}
+mean(tt)
